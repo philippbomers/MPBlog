386674a90158384d366b85191dde991186c259fc
Philipp Bomers <mail@philipp-bomers.de>
2022-04-14 09:28:59 +0200

    Initial commit

---
+++ src/main/java/com/example/mpblog/MPBlogUser.java
@@ -0,0 +1,2 @@
+package com.example.mpblog;public class MPBlogUser {
+}
---
+++ src/main/java/com/example/mpblog/MpBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.mpblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpBlogApplication.class, args);
+    }
+
+}
