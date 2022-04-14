4da7e4ab7b5a22d0d433e0569530377f3b3b32f8
Philipp Bomers <mail@philipp-bomers.de>
2022-04-14 22:31:48 +0200

    bughunting

--- src/main/java/com/example/mpblog/MPBlogController.java
+++ src/main/java/com/example/mpblog/MPBlogController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -29,13 +26,11 @@ public class MPBlogController {
     }
 
     @PostMapping("/registerdialog")
-    public String register(@Valid @ModelAttribute MPBlogUser mpBlogUser, BindingResult bindingResult, Model model) {
+    public String register(@Valid @ModelAttribute("mpBlogUser") MPBlogUser mpBlogUser, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            model.addAttribute("mpBlogUser", mpBlogUser);
             return "registerdialog";
         } else {
             this.mpBlogService.addMPBlogUser(mpBlogUser);
-            model.addAttribute("mpBlogUser", mpBlogUser);
             return "registersuccessfully";
         }
     }
--- src/main/java/com/example/mpblog/entities/MPBlogUser.java
+++ src/main/java/com/example/mpblog/entities/MPBlogUser.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.Valid;
 import java.util.Collection;
 import java.util.List;
 
