53344f61f92220d178b74cede861a8d20c23c3f8
Philipp Bomers <mail@philipp-bomers.de>
2022-04-15 09:39:49 +0200

    cleanup

--- src/main/java/com/example/mpblog/controllers/MPBlogEntryController.java
+++ src/main/java/com/example/mpblog/controllers/MPBlogEntryController.java
@@ -2,7 +2,6 @@ package com.example.mpblog.controllers;
 
 import com.example.mpblog.services.MPBlogEntryService;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/example/mpblog/entities/MPBlogUser.java
+++ src/main/java/com/example/mpblog/entities/MPBlogUser.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.validation.Valid;
 import java.util.Collection;
 import java.util.List;
 
