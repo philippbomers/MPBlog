6eb46bfab7df41a46d53d1c20b1453f2130c24d2
mkatran <mehmetkatran@gmail.com>
2022-04-16 15:33:01 +0200

    Added Get Mapping to delete comments.

--- src/main/java/com/example/mpblog/controllers/MPBlogCommentController.java
+++ src/main/java/com/example/mpblog/controllers/MPBlogCommentController.java
@@ -2,7 +2,9 @@ package com.example.mpblog.controllers;
 
 import com.example.mpblog.entities.MPBlogComment;
 import com.example.mpblog.entities.MPBlogEntry;
+import com.example.mpblog.entities.MPBlogSession;
 import com.example.mpblog.entities.MPBlogUser;
+import com.example.mpblog.repositories.MPBlogCommentRepository;
 import com.example.mpblog.services.MPBlogCommentService;
 import com.example.mpblog.services.MPBlogEntryService;
 import com.example.mpblog.services.MPBlogSessionService;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.util.Optional;
@@ -18,12 +21,13 @@ import java.util.Optional;
 public class MPBlogCommentController {
     private final MPBlogCommentService mpBlogCommentService;
     private final MPBlogEntryService mpBlogEntryService;
-
+    private final MPBlogCommentRepository mpBlogCommentRepository;
     private final MPBlogSessionService mpBlogSessionService;
 
-    public MPBlogCommentController(MPBlogCommentService mpBlogCommentService, MPBlogEntryService mpBlogEntryService, MPBlogSessionService mpBlogSessionService) {
+    public MPBlogCommentController(MPBlogCommentService mpBlogCommentService, MPBlogEntryService mpBlogEntryService, MPBlogCommentRepository mpBlogCommentRepository, MPBlogSessionService mpBlogSessionService) {
         this.mpBlogCommentService = mpBlogCommentService;
         this.mpBlogEntryService = mpBlogEntryService;
+        this.mpBlogCommentRepository = mpBlogCommentRepository;
         this.mpBlogSessionService = mpBlogSessionService;
     }
 
@@ -47,5 +51,17 @@ public class MPBlogCommentController {
 
         model.addAttribute("entry", blogEntry.get());
         return "entrydetails";
+    }
+
+    @GetMapping("/{id}/deleteComment")
+    public String delete(@CookieValue(value = "sessionId", defaultValue = "") String sessionId, @PathVariable int id, Model model) {
+        MPBlogComment comment = mpBlogCommentRepository.findById(id);
+        Optional<MPBlogSession> optionalSession = this.mpBlogSessionService.findById(sessionId);
+        if(comment.getMpBlogUser() != optionalSession.get().getMpBlogUser()) {
+            throw new IllegalArgumentException("User is not authorized to delete this comment!");
+        }
+        int newID = comment.getMpBlogEntry().getId();
+        mpBlogCommentRepository.delete(comment);
+        return "redirect:/" + newID+ "/entrydetails";
     }
 }
