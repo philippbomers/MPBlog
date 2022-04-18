0f20330c2d7b7b746eaf7e62133b3f3d43b4a668
mkatran <mehmetkatran@gmail.com>
2022-04-18 20:43:51 +0200

    Introduced number of entries and comments by user profile.

--- src/main/java/com/example/mpblog/repositories/MPBlogCommentRepository.java
+++ src/main/java/com/example/mpblog/repositories/MPBlogCommentRepository.java
@@ -2,10 +2,12 @@ package com.example.mpblog.repositories;
 
 import com.example.mpblog.entities.MPBlogComment;
 import com.example.mpblog.entities.MPBlogEntry;
+import com.example.mpblog.entities.MPBlogUser;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MPBlogCommentRepository extends CrudRepository<MPBlogComment, Integer> {
@@ -14,4 +16,6 @@ public interface MPBlogCommentRepository extends CrudRepository<MPBlogComment, I
     MPBlogComment findById(int id);
 
     List<MPBlogComment> findByMpBlogEntry(MPBlogEntry blogEntry);
+
+    int countByMpBlogUser(Optional<MPBlogUser> mpBlogUser);
 }
